bb03e3cf20051e516085fd6c60f7d972a8d33fa2
LTC-IT <ryan.cather@ed.act.edu.au>
2022-06-20 22:45:06 +1000

    init commit of base code.

--- RMS/RMS.ino
+++ RMS/RMS.ino
@@ -1,3 +1,26 @@
+#include "sensitiveInformation.h"
+
+#define FORMAT_SPIFFS_IF_FAILED true
+
+// Wifi & Webserver
+#include "WiFi.h"
+#include "SPIFFS.h"
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+AsyncWebServer server(80);
+
+
+// RTC Start - Remove if unnecessary
+#include "RTClib.h"
+
+RTC_PCF8523 rtc;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+// RTC End
+
+boolean LEDOn = false; // State of Built-in LED true=on, false=off.
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -6,4 +29,29 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+
+
+void logEvent(String dataToLog) {
+  /*
+     Log entries to a file stored in SPIFFS partition on the ESP32.
+  */
+  // Get the updated/current time
+  DateTime rightNow = rtc.now();
+  char csvReadableDate[25];
+  sprintf(csvReadableDate, "%02d,%02d,%02d,%02d,%02d,%02d,",  rightNow.year(), rightNow.month(), rightNow.day(), rightNow.hour(), rightNow.minute(), rightNow.second());
+
+  String logTemp = csvReadableDate + dataToLog + "\n"; // Add the data to log onto the end of the date/time
+
+  const char * logEntry = logTemp.c_str(); //convert the logtemp to a char * variable
+
+  //Add the log entry to the end of logevents.csv
+  appendFile(SPIFFS, "/logEvents.csv", logEntry);
+
+  // Output the logEvents - FOR DEBUG ONLY. Comment out to avoid spamming the serial monitor.
+  //  readFile(SPIFFS, "/logEvents.csv");
+
+  Serial.print("\nEvent Logged: ");
+  Serial.println(logEntry);
 }
---
+++ RMS/sensitiveInformation.h
@@ -0,0 +1,12 @@
+/*
+ * Contains any sensitive Infomration that you do not want published to Github.
+ * Makr sure this file is included in the .gitignore!
+ */
+
+const char* host = "ISHP";
+const char* ssid = "RoboRange";        // Wifi Network Name
+const char* password = "Password01";         // Wifi Password
+
+
+const char* http_username = "admin";
+const char* http_password = "admin1";
