69c54a0f08f4bfac2d79d3c81112d1470ec0fb17
LTC-IT <ryan.cather@ed.act.edu.au>
2022-06-20 22:47:20 +1000

    Update sensitiveInformation.h

--- RMS/sensitiveInformation.h
+++ RMS/sensitiveInformation.h
@@ -1,12 +1,16 @@
 /*
  * Contains any sensitive Infomration that you do not want published to Github.
- * Makr sure this file is included in the .gitignore!
+ * 
+ * This is default usernames and passwords that should be changed.
+ * 
+ * Make sure this file is included in the .gitignore!
+ * 
  */
 
-const char* host = "ISHP";
-const char* ssid = "RoboRange";        // Wifi Network Name
-const char* password = "Password01";         // Wifi Password
+const char* host            = "RMS";
+const char* ssid            = "RoboRange";        // Wifi Network Name
+const char* password        = "Password01";         // Wifi Password
 
 
-const char* http_username = "admin";
-const char* http_password = "admin1";
+const char* http_username   = "admin";
+const char* http_password   = "admin";
